51f96e0e3ec7b1aa745ab17a7ab45eb475961649
wojtyfa <benedictus.karol@ti.ukdw.ac.id>
2024-02-16 09:15:42 +0700

    Initial commit

---
+++ soal 2.2.py
@@ -0,0 +1,8 @@
+# latihan 2.2
+x = int(input("Masukkan nilai x (bilangan bulat): "))
+
+def itung_fungsi(x):
+    itung_fungsi = 2 * x**3 + 2 * x + 15 / x
+    return itung_fungsi
+
+print ("hasil" , itung_fungsi(x))
